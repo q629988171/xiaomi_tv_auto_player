a30a3c01243790ed0135243bd6b36711dde11a7a
q629988171 <113960096@qq.com>
2021-04-29 12:07:36 +0800

    Update sketch_attiny85.ino
    修复因为爱奇艺无法开机自启，导致开机不自动播放《萌鸡小队》的问题

--- sketch_attiny85/sketch_attiny85.ino
+++ sketch_attiny85/sketch_attiny85.ino
@@ -1,3 +1,12 @@
+/**
+ * @Author: Allen
+ * @Description: 小米电视开机自动播放《萌鸡小队》
+ * @File: msketch_feb21b.ino
+ * @Version: 1.0.1
+ * @Date: 2021/4/28 16:21
+ */
+
+
 #include "DigiKeyboard.h"
 
 #define KEY_ARROW_DOWN 0x51
@@ -10,11 +19,18 @@ void setup() {
    */
   DigiKeyboard.sendKeyStroke(0);
 
-  for(int i = 1; i <= 5; i++) {
-    delay(1000*30);
+  for(int i = 1; i <= 4; i++) {
+    delay(1000*10);
   }
+  
   keyPressed(KEY_ARROW_DOWN);
-  delay(100);
+  delay(1000);
+  keyPressed(KEY_ARROW_DOWN);
+  delay(1000);
+  keyPressed(KEY_ENTER);
+  delay(3000);
+  keyPressed(KEY_ENTER);
+  delay(5000);
   keyPressed(KEY_ENTER);
 }
 
@@ -22,4 +38,15 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
-  
+
+/**
+ * @Description: 按下键盘上的按键
+ * @param keyStroke
+ * @return none
+ */
+void keyPressed(byte keyStroke){
+  DigiKeyboard.sendKeyStroke(keyStroke);
+  delay(100);
+  // called whenever a pressed key is released on a connected USB keyboard.
+  DigiKeyboard.sendKeyStroke(0);
+}

e26c21e892ad7173062476f468ef101b2f215efa
q629988171 <113960096@qq.com>
2021-04-29 12:11:09 +0800

    Update sketch_leonardo.ino
    修复因为爱奇艺无法开机自启，导致开机不自动播放《萌鸡小队》的问题

--- sketch_leonardo/sketch_leonardo.ino
+++ sketch_leonardo/sketch_leonardo.ino
@@ -1,18 +1,31 @@
+/**
+ * @Author: Allen
+ * @Description: 小米电视开机自动播放《萌鸡小队》
+ * @File: sketch_leonardo.ino
+ * @Version: 1.0.1
+ * @Date: 2021/4/28 16:21
+ */
+ 
 #include <Keyboard.h>
 
-int i = 0;
 
 void setup() {
   Serial.begin(9600);
   //start keyboard communication
   Keyboard.begin();
-  for(i = 1; i <= 4; i++) {
-    delay(1000*30);
+  for(int i = 1; i <= 4; i++) {
+    delay(1000*10);
   }
   //send a keystroke
   sendKeyStroke(KEY_DOWN_ARROW);
-  delay(2000);
+  delay(1000);
+  sendKeyStroke(KEY_DOWN_ARROW);
+  delay(1000);
   sendKeyStroke(KEY_RETURN);
+  delay(3000);
+  sendKeyStroke(KEY_RETURN);
+  delay(5000);
+  sendKeyStroke(KEY_RETURN);  
   //end keyboard communication
   Keyboard.end();
 }
@@ -21,6 +34,11 @@ void loop() {
   //do nothing
 }
 
+/**
+ * @Description: 按下键盘上的按键
+ * @param keyStroke
+ * @return none
+ */
 void sendKeyStroke(byte keyStroke){
   Keyboard.press(keyStroke);
   delay(100);
